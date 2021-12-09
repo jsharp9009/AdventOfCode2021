ad47b11f347ac999af73216f773c13ad60a8b075
Joshua Sharp <jsharp@oldtownit.com>
2021-12-09 15:31:03 -0600

    Days 4 and 5 completed

---
+++ Day 4 - Giant Squid/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Collections.Generic;
+
+namespace GiantSquid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("input.txt").ToList();
+            var DrawNumbers = ReadDrawNumbers(lines[0]);
+
+            lines.RemoveAt(0);
+
+            lines.RemoveAll(l => string.IsNullOrWhiteSpace(l));
+
+            var boards = ReadBoards(lines);
+
+            List<BingoBoard> winningBoards = new List<BingoBoard>();
+            int lastNum = 0;
+            while(DrawNumbers.Count() > 0){
+                lastNum = DrawNumbers.Dequeue();
+
+                boards.ForEach(b => b.MarkSquare(lastNum));
+
+                winningBoards.AddRange(boards.Where(b => b.HasWon()));
+                boards.RemoveAll(b => b.HasWon());
+
+                if(boards.Count() == 0) break;
+            }
+            Console.WriteLine("Last Number Called: " + lastNum);
+            Console.WriteLine("Winning Board Score: " + winningBoards.FirstOrDefault().GetScore());
+
+            Console.WriteLine("Last Winning Board Score: " + winningBoards.LastOrDefault().GetScore());
+        }
+
+        static BingoBoard NewBoard(){
+            var boardSquares = new BoardSquare[5][];
+
+            boardSquares[0] = new BoardSquare[5] {new BoardSquare(), new BoardSquare(), new BoardSquare(), new BoardSquare(), new BoardSquare()};
+            boardSquares[1] = new BoardSquare[5]{new BoardSquare(), new BoardSquare(), new BoardSquare(), new BoardSquare(), new BoardSquare()};
+            boardSquares[2] = new BoardSquare[5]{new BoardSquare(), new BoardSquare(), new BoardSquare(), new BoardSquare(), new BoardSquare()};
+            boardSquares[3] = new BoardSquare[5]{new BoardSquare(), new BoardSquare(), new BoardSquare(), new BoardSquare(), new BoardSquare()};
+            boardSquares[4] = new BoardSquare[5]{new BoardSquare(), new BoardSquare(), new BoardSquare(), new BoardSquare(), new BoardSquare()};
+
+            return new BingoBoard(){ Board = boardSquares};
+        }
+
+        static Queue<int> ReadDrawNumbers(string input){
+            var numbers = input.Split(',');
+            var queue = new Queue<int>();
+
+            foreach(var num in numbers){
+                queue.Enqueue(Convert.ToInt32(num.Trim()));
+            }
+
+            return queue;
+        }
+
+        static List<BingoBoard> ReadBoards(List<string> lines){
+            List<BingoBoard> boards = new List<BingoBoard>();
+            for(int i = 0; i < lines.Count(); i++){
+                if(i % 5  == 0)
+                    boards.Add(NewBoard());
+                for(int n = 0; n < lines[i].Length; n+=3){
+                    var stringNum = lines[i][n].ToString() + lines[i][n+1].ToString();
+                    var intNum = Convert.ToInt32(stringNum.Trim());
+                    boards.Last().Board[i%5][n/3].Number = intNum;
+                }
+            }
+            return boards;
+        }
+    }
+
+    class BoardSquare{
+        public int Number {get;set;}
+        public bool Marked {get;set;} = false;
+    }
+
+    class BingoBoard {
+        public BoardSquare[][] Board {get;set;}
+
+        public int GetScore(){
+            return this.Board.Sum(r => r.Sum(s => s.Marked ? 0 : s.Number));
+        }
+
+        public void MarkSquare(int number){
+            var squares = (from row in Board
+                        from square in row
+                        where square.Number == number
+                        select square).ToList();
+            squares.ForEach(s => s.Marked = true);
+        }
+
+        public bool HasWon(){
+            if(Board.Any(r => r.All(b => b.Marked)))
+                return true;
+
+            for(int i = 0; i < Board[0].Count(); i++){
+                if(Board.All(r => r[i].Marked)){
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Day 5 - Hydrothermal Venture/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace HydrothermalVenture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("input.txt");
+            var lines = GetLines(input);
+
+            var straightLines = lines.Where(l => l.Start.X == l.End.X || l.Start.Y == l.End.Y).ToList();
+
+            var allPoints = new List<Vector2>();
+
+            straightLines.ForEach(l => allPoints.AddRange(l.AllPoints));
+
+            var crosses = allPoints.GroupBy(x => ComputeHash(x));
+
+            Console.WriteLine("Crosses " + crosses.Where(c => c.Count() > 1).Count() + " times with only horizontal and vertical lines");
+
+            allPoints.Clear();
+
+            lines.ForEach(l => allPoints.AddRange(l.AllPoints));
+
+            crosses = allPoints.GroupBy(x => ComputeHash(x));
+
+            Console.WriteLine("Crosses " + crosses.Where(c => c.Count() > 1).Count() + " times for all lines");
+        }
+
+        static List<Line> GetLines(string[] lines){
+            var inputLines = new List<Line>();
+            foreach(var inputLine in lines){
+                var startEnd = inputLine.Split(" -> ");
+                var startSplit = startEnd[0].Split(",");
+                var endSplit = startEnd[1].Split(",");
+
+                var startPoint = new Vector2(float.Parse(startSplit[0]), float.Parse(startSplit[1]));
+                var endPoint = new Vector2(float.Parse(endSplit[0]), float.Parse(endSplit[1]));
+
+                inputLines.Add(new Line(startPoint, endPoint));
+            }
+
+            return inputLines;
+        }
+
+        static long ComputeHash(Vector2 point){
+            return Convert.ToInt64(LegthenString(point.X.ToString()) + LegthenString(point.Y.ToString()));
+        }
+
+        static string LegthenString(string number){
+            while(number.Length < 3){
+                number = "0" + number;
+            }
+            return number;
+        }
+    }
+
+    class Line
+    {
+        public Vector2 Start { get; private set; }
+        public Vector2 End { get; private set; }
+        public Vector2 Step { get; private set; }
+        public List<Vector2> AllPoints { get; private set; }
+
+        public Line(Vector2 start, Vector2 end)
+        {
+            Start = start;
+            End = end;
+
+            Step = CalculateStep();
+
+            AllPoints = GetAllPoints();
+        }
+
+        private Vector2 CalculateStep()
+        {
+            var x = End.X - Start.X;
+            var y = End.Y - Start.Y;
+
+            var gcd = Math.Abs(GCD(x, y));
+
+            return new Vector2() { X = x / gcd, Y = y / gcd };
+        }
+
+        static float GCD(float a, float b)
+        {
+            return b == 0 ? a : GCD(b, a % b);
+        }
+
+        List<Vector2> GetAllPoints(){
+            var allPoints = new List<Vector2>();
+            allPoints.Add(Start);
+            var nextPoint = Vector2.Add(allPoints.Last(), Step);
+            while(nextPoint.X != End.X || nextPoint.Y != End.Y){
+                allPoints.Add(nextPoint);
+                nextPoint = Vector2.Add(allPoints.Last(), Step);
+            }
+            allPoints.Add(End);
+
+            return allPoints;
+        }
+    }
+}
