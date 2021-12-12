a23d7b2433aec2652e8188bbf817cc03569dd166
jsharp9009 <jsharp9009@live.com>
2021-12-11 22:55:54 -0600

    Day 9 Complete

--- Day 9 - Smoke Basin/Program.cs
+++ Day 9 - Smoke Basin/Program.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Linq;
 using System.Collections.Generic;
+using System.Numerics;
 
 namespace SmokeBasin
 {
@@ -19,7 +20,7 @@ namespace SmokeBasin
             }
 
             int total = 0;
-            var lowPoints = new List<object>();
+            var lowPoints = new List<Vector2>();
 
             for(int i = 0; i < heightmap.GetLength(0); i++){
                 for(int n = 0; n < heightmap.GetLength(1); n++){
@@ -46,6 +47,7 @@ namespace SmokeBasin
 
                     if(isLowpoint){
                         total+=(current + 1);
+                        lowPoints.Add(new Vector2(i,n));
                         Console.ForegroundColor = ConsoleColor.Green;
                         Console.Write(current);
                         Console.ResetColor();
@@ -64,7 +66,42 @@ namespace SmokeBasin
                 Console.WriteLine("");
             }
 
+            List<List<Vector2>> basins = new List<List<Vector2>>();
+
+            foreach(var lowpoint in lowPoints){
+                var basin = new List<Vector2>();
+                var max = new Vector2(heightmap.GetLength(0), heightmap.GetLength(1));
+
+                GetBasin(heightmap, lowpoint, basin, max);
+
+                basins.Add(basin);
+            }
+
                 Console.WriteLine("Total Lowpoint Risk Level: " + total);
+                
+                var basinTotal = basins.OrderByDescending(b => b.Count()).Take(3).Select(t => t.Count()).Aggregate(1, (a, b) => a * b);
+
+                Console.WriteLine("Basin Total: " + basinTotal);
+        }
+
+        static void GetBasin(int[,] heightmap, Vector2 point, List<Vector2> basin, Vector2 max)
+        {
+            if(heightmap[(int)point.X, (int)point.Y] == 9)
+                return;
+
+            basin.Add(point);
+            if((point.X - 1) >= 0 && !basin.Any(b => b.X == (point.X - 1) && b.Y == point.Y)){
+                GetBasin(heightmap, new Vector2(point.X - 1, point.Y), basin, max);
+            }
+            if((point.X + 1) < max.X  && !basin.Any(b => b.X == (point.X + 1) && b.Y == point.Y)){
+                GetBasin(heightmap, new Vector2(point.X + 1, point.Y), basin,max);
+            }
+            if((point.Y - 1) >= 0  && !basin.Any(b => b.X == point.X && b.Y == (point.Y - 1))){
+                GetBasin(heightmap, new Vector2(point.X, point.Y - 1), basin,max);
+            }
+            if((point.Y + 1) < max.Y  && !basin.Any(b => b.X == point.X && b.Y == (point.Y + 1))){
+                GetBasin(heightmap, new Vector2(point.X, point.Y + 1), basin,max);
+            }
         }
     }
 }
