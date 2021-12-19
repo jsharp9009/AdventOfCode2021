92305ab0d490f135a5226aebae1471457eb29e00
Joshua Sharp <jsharp@oldtownit.com>
2021-12-19 16:11:56 -0600

    Day 17 Complete!

--- Day 17 - Trick Shot/Program.cs
+++ Day 17 - Trick Shot/Program.cs
@@ -8,51 +8,63 @@ namespace TrickShot
 {
     class Program
     {
-        static int targetMinX = 20;
-            static int targetMaxX = 30;
-            static int targetMinY = -10;
-            static int targetMaxY = -5;
+        static int targetMinX = 150;
+            static int targetMaxX = 171;
+            static int targetMinY = -70;
+            static int targetMaxY = -129;
         static void Main(string[] args)
         {
             List<int> maxYs = new List<int>();
 
-            for(int x = 1; x < 10; x++){
-                for(int y = 1; y < 10; y++){
-                    maxYs.Add(TestStep(x, y).Y);
+            var minX = MinimunX(targetMinX);
+
+            var hit = 0;
+
+            for(int x = minX; x <= targetMaxX; x++){
+                for(int y = targetMaxY; y < -targetMaxY; y++){
+                    var maxY =TestStep(x, y);
+                    if(maxY != null) {
+                        maxYs.Add(maxY.Value);
+                        //Console.WriteLine(x + "," + y);
+                        hit++;
+                    }
                 }
             }
 
             Console.WriteLine("Max Y: " + maxYs.Max());
+            Console.WriteLine("Intitial Velocities that Hit: " + hit);
         }
 
-        static dynamic TestStep(int xStep, int yStep){
-            var currentPostion = new Vector2(0, 0);
+        static int? TestStep(int xStep, int yStep){
+            var currentX = 0;
+            var currentY = 0;
             var maxY = -1;
-            while(currentPostion.X < targetMaxX && currentPostion.Y > targetMinY){
-                currentPostion = new Vector2(currentPostion.X + xStep, currentPostion.Y + yStep);
-                
-                if(currentPostion.Y > maxY){
-                    maxY = (int)currentPostion.Y;
+            while(currentX <= targetMaxX && currentY >= targetMaxY){
+                currentX += xStep;
+                if(xStep > 0)
+                    xStep--;
+
+                currentY += yStep--;
+                if(currentY > maxY){
+                    maxY = currentY;
                 }
 
-                if(currentPostion.X > 0){
-                    currentPostion = new Vector2(currentPostion.X - 1, currentPostion.Y - 1);
-                }
-                else if(currentPostion.X < 0){
-                    currentPostion = new Vector2(currentPostion.X + 1, currentPostion.Y - 1);
-                }
-                else{
-                    currentPostion = new Vector2(currentPostion.X, currentPostion.Y - 1);
-                }
-
-                if(currentPostion.X > targetMinX && currentPostion.X < targetMaxX
-                    && currentPostion.Y < targetMinY && currentPostion.Y > targetMaxY){
-                        return new {Y = maxY};
+                if(currentX >= targetMinX && currentX <= targetMaxX
+                    && currentY <= targetMinY && currentY >= targetMaxY){
+                        return maxY;
                     }
 
                 
             }
-            return new {Y = -1};
+            return null;
+        }
+
+        static int MinimunX(int leftBound)
+        {
+            var i = 0; 
+            for (; i * (i + 1) < 2 * leftBound; i++)
+                ;
+            return i;
         }
     }
 }
