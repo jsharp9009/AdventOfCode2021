7f8185bbc284398904a177f13b3b95b3aaecb349
Joshua Sharp <jsharp@oldtownit.com>
2022-01-06 10:51:13 -0600

    Day 20 finally working!

--- Day 20 - Trench Map/Program.cs
+++ Day 20 - Trench Map/Program.cs
@@ -16,14 +16,19 @@ namespace TrenchMap
 
             //DrawImage(image);
             for(int i = 0; i < 2; i++){
-                image = ProcessImage(image, algorithm);
+                image = ProcessImage(image, algorithm, i % 2 == 0);
+                //image = DeExtendImage(image, 1);
             }
 
-            image = DeExtendImage(image);
+            //image = DeExtendImage(image, 2);        
 
-            DrawImage(image);
+            Console.WriteLine("Light up after 2: " + image.SelectMany(s => s).Aggregate((t, b) => t += b));
 
-            Console.WriteLine("Light up: " + image.SelectMany(s => s).Aggregate((t, b) => t += b));
+            for(int i = 0; i < 48; i++){
+                image = ProcessImage(image, algorithm, i % 2 == 0);
+            }
+
+            Console.WriteLine("Light up after 50: " + image.SelectMany(s => s).Aggregate((t, b) => t += b));
         }
 
         static List<int> ReadEhancementAlgorithm(string input){
@@ -58,52 +63,57 @@ namespace TrenchMap
             return image;
         }
 
-        static List<List<int>> ExtendImage(List<List<int>> image){
+        static List<List<int>> ExtendImage(List<List<int>> image, List<int> algorithm, bool isOdd){
+            var toAdd = !isOdd ? algorithm[0] : 0;
+            
             foreach(var line in image){
-                line.AddRange(new int[]{0, 0});
-                line.InsertRange(0, new int[]{0, 0});
+                line.AddRange(new int[]{toAdd, toAdd});
+                line.InsertRange(0, new int[]{toAdd, toAdd});
             }
 
             for(int i = 0; i < 2; i++){
-                image.Add(NewZeroList(image[0].Count));
-                image.Insert(0, NewZeroList(image[0].Count));
+                image.Add(NewIntList(toAdd, image[0].Count));
+                image.Insert(0, NewIntList(toAdd, image[0].Count));
             }
 
             return image;
         }
 
-        static List<List<int>> DeExtendImage(List<List<int>> image){
-            image.RemoveRange(0, 1);
-            image.RemoveRange(image.Count - 1, 1);
+        static List<List<int>> DeExtendImage(List<List<int>> image, int length){
+            image.RemoveRange(0, length);
+            image.RemoveRange(image.Count - length, length);
             foreach(var line in image){
-                line.RemoveRange(0, 1);
-                line.RemoveRange(line.Count - 1, 1);
+                line.RemoveRange(0, length);
+                line.RemoveRange(line.Count - length, length);
             }
             return image;
         }
 
-        static List<int> NewZeroList(int size){
+        static List<int> NewIntList(int toAdd, int size){
             var ret = new List<int>();
             for(int i = 0; i < size; i++){
-                ret.Add(0);
+                ret.Add(toAdd);
             }
             return ret;
         }
 
-        static List<List<int>> NewImage(int height, int width){
+        static List<List<int>> NewImage(int height, int width, List<int> algorithm, bool isOdd){
             var ret = new List<List<int>>();
+
+            var toAdd = isOdd ? algorithm[0] : 0;
+
             for(int i = 0; i < height; i++){
                 var list = new List<int>();
-                list.AddRange(NewZeroList(width));
+                list.AddRange(NewIntList(toAdd, width));
                 ret.Add(list);
             }
             return ret;
         }
 
-        static List<List<int>> ProcessImage(List<List<int>> image, List<int> algorithm){
-            var extended = ExtendImage(image);
+        static List<List<int>> ProcessImage(List<List<int>> image, List<int> algorithm, bool isOdd){
+            var extended = ExtendImage(image, algorithm, isOdd);
             //DrawImage(extended);
-            var newImage = NewImage(extended.Count, extended[0].Count);
+            var newImage = NewImage(extended.Count, extended[0].Count, algorithm, isOdd);
 
             for(int i = 1; i < extended.Count - 1; i++){
                 for(int n = 1; n < extended[i].Count - 1; n++){
